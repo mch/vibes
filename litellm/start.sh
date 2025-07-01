07ecb7a57c696ef317398feaed33e09a3a0541d9
Matt Hughes <matt.c.hughes@gmail.com>
2025-07-01 10:25:45 -0600

    Run litellm using docker

--- litellm/start.sh
+++ litellm/start.sh
@@ -2,11 +2,13 @@
 
 export GEMINI_API_KEY=$(op item get vg55xz2n6pfdkhrljiupfrlpfq --fields "credential" --reveal)
 
+VERSION="main-v1.73.6.rc.1"
+
 docker run \
-    -v $(pwd)/config.yaml:/app/config.yaml \
+    -v $(pwd)/litellm_config.yaml:/app/config.yaml \
     -e GEMINI_API_KEY=${GEMINI_API_KEY} \
     -p 4000:4000 \
-    ghcr.io/berriai/litellm:main-latest \
+    ghcr.io/berriai/litellm:${VERSION} \
     --config /app/config.yaml --detailed_debug
 
 # RUNNING on http://0.0.0.0:4000
